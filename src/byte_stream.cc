4b315877af599336773ac182fddde6fe2681841c
meowrain <meowrain@126.com>
2024-04-20 23:06:02 +0800

    check: 👍byte_stream通过测试

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -7,59 +7,81 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 bool Writer::is_closed() const
 {
   // Your code here.
-  return {};
+  return closed_;
 }
 
-void Writer::push( string data )
-{
-  // Your code here.
-  (void)data;
-  return;
+void Writer::push( string data ) {
+  //如果流已经关闭或者是有错误发生，那么就设置error为true，然后退出程序
+  if ( closed_ || error_ ) {
+    set_error();
+    return;
+  }
+
+  if (data.length() > available_capacity()) {//如果插入数据长度大于可用的容量，那么就截断数据
+    data = data.substr(0, available_capacity()); // 截断数据以适应可用容量，并修改data为截断后的数据
+  }
+  //在双端队列中插入数据，使用copy函数可实现拷贝string中的数据到双端队列buffer_中
+  //std::back_inserter 是一个插入迭代器，它提供一种通用的方式来将元素插入到容器的末尾。在这个场景下，每当 std::copy 算法尝试通过迭代器写入一个元素时，
+  //std::back_inserter 调用双端队列的 push_back 方法，将新元素添加到队列的末尾。
+  std::copy( data.begin(), data.end(), std::back_inserter( buffer_ ) );
+  //写入完成后，把写入的字节数加上data的大小
+  bytes_written_ += data.size();
 }
 
 void Writer::close()
 {
+  closed_ = true;
   // Your code here.
 }
 
 uint64_t Writer::available_capacity() const
 {
+
   // Your code here.
-  return {};
+  uint64_t ava_capacity = capacity_ - buffer_.size();
+  return ava_capacity;
 }
 
 uint64_t Writer::bytes_pushed() const
 {
   // Your code here.
-  return {};
+  return bytes_written_;
 }
 
 bool Reader::is_finished() const
 {
   // Your code here.
-  return {};
+  bool res = closed_ && buffer_.empty();
+  return res;
 }
 
 uint64_t Reader::bytes_popped() const
 {
   // Your code here.
-  return {};
+  return bytes_read_;
 }
 
 string_view Reader::peek() const
 {
   // Your code here.
-  return {};
+  if ( !buffer_.empty() ) {
+    return std::string_view( &buffer_.front(), 1 );
+  }
+  return std::string_view();
 }
 
 void Reader::pop( uint64_t len )
 {
   // Your code here.
-  (void)len;
+  if ( len > buffer_.size() ) {
+    len = buffer_.size();
+  }
+  buffer_.erase( buffer_.begin(), buffer_.begin() + len );
+  bytes_read_ += len;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
   // Your code here.
-  return {};
+  return buffer_.size();
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -3,7 +3,9 @@
 #include <cstdint>
 #include <string>
 #include <string_view>
-
+#include <deque>
+#include <iostream>
+#include <algorithm>
 class Reader;
 class Writer;
 
@@ -12,41 +14,63 @@ class ByteStream
 public:
   explicit ByteStream( uint64_t capacity );
 
-  // Helper functions (provided) to access the ByteStream's Reader and Writer interfaces
+  // 访问 ByteStream 的 Reader 和 Writer 接口的辅助函数（已提供）
   Reader& reader();
   const Reader& reader() const;
   Writer& writer();
   const Writer& writer() const;
 
-  void set_error() { error_ = true; };       // Signal that the stream suffered an error.
-  bool has_error() const { return error_; }; // Has the stream had an error?
+  // 发出流发生错误的信号。
+  void set_error() { error_ = true; };
+  // 返回流是否有错误
+  bool has_error() const { return error_; };
 
 protected:
-  // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
+  // 请将任何附加状态添加到此处的 ByteStream，而不是添加到 Writer 和 Reader 接口。
+  //使用deque双端队列存储字节
+  std::deque<char> buffer_ {};
+  //容量
   uint64_t capacity_;
+  //错误默认初始化为false
   bool error_ {};
+  //流默认状态为false，也就是打开状态
+  bool closed_ {};
+  //写入的字节数
+  uint64_t bytes_written_ {};
+  //读取的字节数
+  uint64_t bytes_read_ {};
 };
 
 class Writer : public ByteStream
 {
 public:
-  void push( std::string data ); // Push data to stream, but only as much as available capacity allows.
-  void close();                  // Signal that the stream has reached its ending. Nothing more will be written.
+  // 将数据推送到流中，但仅限于可用容量允许的数量。
+  void push( std::string data );
+  // 指示流已到达结尾。不会再写更多的了。
+  void close();
 
-  bool is_closed() const;              // Has the stream been closed?
-  uint64_t available_capacity() const; // How many bytes can be pushed to the stream right now?
-  uint64_t bytes_pushed() const;       // Total number of bytes cumulatively pushed to the stream
+  // 返回流是否已关闭
+  bool is_closed() const;
+  // 现在可以将多少字节推送到流中？
+  uint64_t available_capacity() const;
+  // 累计推送到流的总字节数
+  uint64_t bytes_pushed() const;
 };
 
 class Reader : public ByteStream
 {
 public:
-  std::string_view peek() const; // Peek at the next bytes in the buffer
-  void pop( uint64_t len );      // Remove `len` bytes from the buffer
+  // 查看缓冲区中的下一个字节
+  std::string_view peek() const;
+  // 从缓冲区中删除 `len` 字节
+  void pop( uint64_t len );
 
-  bool is_finished() const;        // Is the stream finished (closed and fully popped)?
-  uint64_t bytes_buffered() const; // Number of bytes currently buffered (pushed and not popped)
-  uint64_t bytes_popped() const;   // Total number of bytes cumulatively popped from stream
+  // 流是否已完成（关闭并完全弹出）？
+  bool is_finished() const;
+  // 当前缓冲的字节数（推送且未弹出）
+  uint64_t bytes_buffered() const;
+  // 从流中累积弹出的字节总数
+  uint64_t bytes_popped() const;
 };
 
 /*
